181ff2fd8de82094d25f164a0912038c6a694df4
CodeDev9 <127266511+codedev9@users.noreply.github.com>
2024-12-07 00:20:50 +0000

    lol

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 script for busy business
 # Script
 ```lua
-local repo2 = "https://raw.githubusercontent.com/codedev9/BusyBusiness/refs/heads/main/"
-loadstring(game:HttpGet(repo2.."script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/codedev9/BusyBusiness/refs/heads/main/script.lua"))()
 ```
--- loader.lua
+++ loader.lua
@@ -4,7 +4,6 @@ local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 local Options = Library.Options
 local Toggles = Library.Toggles
-local repo2 = "https://raw.githubusercontent.com/codedev9/BusyBusiness/refs/heads/main/"
 --//
 if game.GameId == 10914683361 then 
     Library:Notify("Busy Business Loader - Loading Script...", nil, 4590657391)
@@ -13,7 +12,7 @@ if game.GameId == 10914683361 then
     wait(1)
     Library:Notify("Busy Business Loader - Finished Loading", nil, 4590657391)
     wait(1)
-    loadstring(game:HttpGet(repo2.."script.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/codedev9/BusyBusiness/refs/heads/main/script.lua"))()
 else
     Library:Notify("Busy Business Loader - Game not supported.", nil, 4590657391)
     return
